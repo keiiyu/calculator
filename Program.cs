732abe261d997cd216363999a9170e21c9364cbd
keiiyu <tsukishimaritsukei@gmail.com>
2026-04-08 23:46:45 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ static int Pembagian(int var1, int var2)
 }
 
 Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + Penjumlahan(var1, var2));
-Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2));
+Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2);
 Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + Perkalian(var1, var2));
 Console.WriteLine("Pembagian: " + var1 + " / " + var2 + " = " + Pembagian(var1, var2));

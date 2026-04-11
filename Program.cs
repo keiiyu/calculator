fe6ee397ff73de52ebc855af0ad07e4702f38e0e
keiiyu <tsukishimaritsukei@gmail.com>
2026-04-11 11:51:30 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,3 +17,7 @@ static int Pembagian(int var1, int var2)
 {
     return var1 / var2;
 }
+Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + Penjumlahan(var1, var2));
+Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2);
+Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + Perkalian(var1, var2));
+Console.WriteLine("Pembagian: " + var1 + " / " + var2 + " = " + Pembagian(var1, var2));
